1c5ba072ea081d152be3c6953256c37b5315de9c
breno085 <br3noacioli@gmail.com>
2024-07-17 22:49:03 -0300

    resolucao

---
+++ Resolucao - Consultas.sql
@@ -0,0 +1,62 @@
+SELECT * FROM Atores
+
+SELECT * FROM ElencoFilme
+
+SELECT * FROM Filmes
+
+SELECT * FROM FilmesGenero
+
+SELECT * FROM Generos
+
+-- 1
+SELECT Nome, Ano FROM Filmes
+
+-- 2
+SELECT Nome, Ano, Duracao FROM Filmes ORDER BY Ano
+
+-- 3
+SELECT Nome, Ano, Duracao FROM Filmes WHERE Nome = 'De Volta para o Futuro'
+
+-- 4
+SELECT Nome, Ano, Duracao FROM Filmes WHERE Ano = 1997
+
+-- 5
+SELECT Nome, Ano, Duracao FROM Filmes WHERE Ano > 2000
+
+-- 6
+SELECT Nome, Ano, Duracao FROM Filmes WHERE Duracao < 150 AND Duracao > 100 ORDER BY Duracao
+
+-- 7
+SELECT Ano, COUNT(*) AS Quantidade FROM Filmes GROUP BY Ano ORDER BY Quantidade DESC
+
+-- 8
+SELECT * FROM Atores WHERE Genero = 'M'
+
+-- 9
+SELECT * FROM Atores WHERE Genero = 'F' ORDER BY PrimeiroNome, UltimoNome
+
+-- 10
+SELECT Filmes.Nome, Generos.Genero
+FROM Filmes
+INNER JOIN 
+FilmesGenero ON Filmes.Id = FilmesGenero.IdFilme
+INNER JOIN
+Generos ON Generos.Id = FilmesGenero.IdGenero
+
+-- 11
+SELECT Filmes.Nome, Generos.Genero
+FROM Filmes
+INNER JOIN 
+FilmesGenero ON Filmes.Id = FilmesGenero.IdFilme
+INNER JOIN
+Generos ON Generos.Id = FilmesGenero.IdGenero
+WHERE Genero = 'Mistério'
+
+-- 12
+SELECT Filmes.Nome, Atores.PrimeiroNome, Atores.UltimoNome, ElencoFilme.Papel
+FROM Filmes
+INNER JOIN
+ElencoFilme ON ElencoFilme.IdFilme = Filmes.Id
+INNER JOIN
+Atores ON Atores.Id = ElencoFilme.IdAtor
+
